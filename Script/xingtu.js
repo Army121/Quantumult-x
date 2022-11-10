4bfd381d051781bbe4dc6591285bf8aa97f7998c
Army121 <89560437+Army121@users.noreply.github.com>
2022-11-10 10:53:21 +0800

    Create xingtu.js

---
+++ Script/xingtu.js
@@ -0,0 +1,21 @@
+/*******************************
+醒图vip
+脚本名称:醒图vip
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
+
+[rewrite_local]
+^http[s]?:\/\/commerce-api.faceu.mobi\/commerce\/v1\/subscription\/user_info.+$ url script-response-body xingtu.js
+
+[mitm] 
+hostname = *.faceu.*
+
+*******************************/
+
+var obj = JSON.parse($response.body);
+    obj.data.flag= true;
+obj.data.end_time= 1843114830780;
+obj.data.start_time=  1653726030780;
+    $done({body: JSON.stringify(obj)});
